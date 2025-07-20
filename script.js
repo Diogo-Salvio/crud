823b7302e65020a28366cd007e023a467d3dad25
Diogo Salvio <diogosalvio787@gmail.com>
2025-07-20 19:34:21 -0300

    Criação da função createTask().

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const openBtn = document.getElementById('openModal');
 const closeBtn = document.getElementById('closeModal');
 const modal = document.getElementById('modal');
 
-
+const tasks = []
 
 
 openBtn.addEventListener('click', () => {
@@ -14,5 +14,52 @@ closeBtn.addEventListener('click', () => {
 })
 
 modal.addEventListener('click', (event) => {
-    console.log(event)
+    if (event.target === modal){
+        modal.classList.remove("open");
+    }
 })
+
+
+function createTask() {
+    const title = document.getElementById('title');
+    const date = document.getElementById('date');
+    const description = document.getElementById('description');
+    const newTask = {title: title.value, date: date.value, description: description.value};
+    
+    const containerCards = document.getElementById('pendingtasks')
+    const newCard = document.createElement('div');
+    newCard.classList.add('card');
+    
+    const heading = document.createElement('h3');
+    heading.textContent = newTask.title;
+    newCard.appendChild(heading);
+
+    const spanDate = document.createElement('span');
+    spanDate.textContent = 'Data da tarefa:';
+    newCard.appendChild(spanDate);
+
+    const inputDate = document.createElement('input');
+    inputDate.type = 'date';
+    inputDate.value = newTask.date;
+    newCard.appendChild(inputDate);
+
+    const descriptionP = document.createElement('p');
+    descriptionP.textContent = newTask.description;
+    newCard.appendChild(descriptionP);
+
+    const buttonRemove = document.createElement('button');
+    buttonRemove.innerHTML = "Excluir Tarefa"
+    newCard.appendChild(buttonRemove);
+
+    const buttonEdit = document.createElement('button');
+    buttonEdit.innerHTML = "Editar Tarefa"
+    newCard.appendChild(buttonEdit);
+    
+    containerCards.appendChild(newCard)
+    
+    
+    tasks.push(newTask);
+    console.log(tasks);
+
+
+}
